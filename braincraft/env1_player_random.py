a32d32b943f6a573f270057b7c58ce5a2c3a765b
GuanchunLi <liguanchun@outlook.com>
2026-04-08 11:06:18 -0500

    Improve plotting

--- braincraft/env1_player_random.py
+++ braincraft/env1_player_random.py
@@ -4,9 +4,11 @@
 """
 Example and evaluation of the performances of a random player.
 """
+import numpy as np
 from bot import Bot
 from environment_1 import Environment
-    
+from challenge_1 import evaluate
+
 def identity(x):
     return x
 
@@ -48,8 +50,7 @@ def random_player():
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
     import time
-    import numpy as np    
-    from challenge_1 import train, evaluate
+    from challenge_1 import train
 
     seed = 12345
     
